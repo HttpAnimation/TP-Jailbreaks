65120fec6692dba3fbda9875e3993159ce9fee5d
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-12 10:38:52 -0500

    Update GitInstall.bash

--- GitInstall.bash
+++ GitInstall.bash
@@ -1,6 +1,7 @@
 echo "Git installer"
 echo "Script by HttpAnimation"
 echo "======================="
+cd ~/Desktop
 mkdir TP-Jailbreks
 cd TP-Jailbreks
 git clone -b main https://github.com/HttpAnimation/TP-Jailbreks.git
