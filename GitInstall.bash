d2432d0c36d20c22af31581ed864948be97b174c
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-12 10:37:55 -0500

    Create GitInstall.bash

---
+++ GitInstall.bash
@@ -0,0 +1,8 @@
+echo "Git installer"
+echo "Script by HttpAnimation"
+echo "======================="
+
+git clone -b main https://github.com/HttpAnimationTP-Jailbreks.git
+mv TP-Jailbreks main
+git clone -b gh-pages https://github.com/HttpAnimationTP-Jailbreks.gi
+mv TP-Jailbreks gh-pages t
